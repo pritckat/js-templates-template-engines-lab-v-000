21c4df27338b04e555cf0e75903051e874d72414
Katrina Pritchard <pritckat@gmail.com>
2018-10-15 18:43:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function createPost() {
-
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementsById("post-template").innerHTML);
 }
